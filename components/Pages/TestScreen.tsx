3060a38c01dfe4b9111d55860ddb5fe380a97308
Upendra Yadav <upendra@vruttitech.com>
2024-02-29 09:53:55 +0530

    chore : websocket link changed

--- components/Pages/TestScreen.tsx
+++ components/Pages/TestScreen.tsx
@@ -411,8 +411,8 @@ const TestScreen = () => {
   };
 
   useEffect(() => {
-    // const socket = new WebSocket('ws://192.168.4.1:80/ws');
-    const socket = new WebSocket('ws://192.168.10.19:8080');
+    const socket = new WebSocket('ws://192.168.4.1:80/ws');
+    // const socket = new WebSocket('ws://192.168.10.19:8080');
     socket.onopen = () => {
       ToastAndroid.show('Connection Successful', ToastAndroid.LONG);
       setSocketConnected(true);
